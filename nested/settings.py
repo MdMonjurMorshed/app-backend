2468c58bf321ba1e70c686efe442ee470a41b0bb
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2023-01-08 20:52:10 +0600

    static root

--- nested/settings.py
+++ nested/settings.py
@@ -127,12 +127,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT='nestedapp/static'
 
 
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'nestedapp/media')
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'nestedapp/static',)]
+
 
 
 
