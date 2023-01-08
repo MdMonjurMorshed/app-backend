7fad52ae09058f7561bcda1c428148ed0c1fb592
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2023-01-08 20:48:09 +0600

    static root

--- nested/settings.py
+++ nested/settings.py
@@ -132,7 +132,7 @@ STATIC_URL = '/static/'
 MEDIA_URL='/media/'
 MEDIA_ROOT=os.path.join(BASE_DIR,'nestedapp/media')
 
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'static',)]
+STATICFILES_DIRS=[os.path.join(BASE_DIR,'nestedapp/static',)]
 
 
 
