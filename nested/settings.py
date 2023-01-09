abb37a70c56c4e106aa4b6ee19100f42c4b8acd9
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2023-01-09 12:39:14 +0600

    app templates

--- nested/settings.py
+++ nested/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'nested.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': os.path.join(BASE_DIR, 'templates','adminlte'),
+        'DIRS': os.path.join(BASE_DIR,'nestedapp', 'templates','adminlte'),
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
