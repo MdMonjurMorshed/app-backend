b87a565c75416d0462a5369687cda9c7242122c6
MD MONJUR MORSHED <morshed.naptechlabs@gmail.com>
2023-01-09 11:35:29 +0600

    host change

--- nested/settings.py
+++ nested/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-giqd*_+-5(ob#zx_t@sc$$kn_283k9_rmktgs$b!khfc!^kkzy
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://backend-app.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['backend-app.herokuapp.com','127.0.0.1']
 
 
 # Application definition
