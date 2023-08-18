55b9d43c0801da3367b8329719b5a8c29851d46a
MD MONJUR MORSHED <mmt3719@gmail.com>
2023-08-18 22:15:26 +0600

    new change

--- nested/settings.py
+++ nested/settings.py
@@ -94,11 +94,11 @@ WSGI_APPLICATION = 'nested.wsgi.application'
 
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'd3le8c4hmmdcak',
-        'USER': 'opwyflnuivnkwh',
-        'PASSWORD': '4331ccec8c529e1560136f5e14d52f9e449665cb57753fcb947ce09ad3f51a55',
-        'HOST': 'ec2-54-86-214-124.compute-1.amazonaws.com',
+        'ENGINE':os.environ.get('db_engine') ,
+        'NAME': os.environ.get('db_name'),
+        'USER': os.environ.get('db_user'),
+        'PASSWORD': os.environ.get('db_pass'),
+        'HOST': os.environ.get('db_host'),
         'PORT': '5432',
 }
 }
